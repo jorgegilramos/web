61a918d31c9950453962ec454f5b2d27b81e27cb
jorgegilramos <jorgegilramos@gmail.com>
2016-02-14 22:04:22 +0100

    Platform

--- assets/js/views/intro.js
+++ assets/js/views/intro.js
@@ -1,9 +1,11 @@
 var Backbone        = require('backbone');
 var _               = require('underscore');
 var $               = require('jquery');
+var Platform        = require('platform');
 
 var IntroView = Backbone.View.extend({
   initialize: function(options){
+    $('html').addClass('platform-' + Platform.name.toLowerCase());
     this.appModel = options.appModel;
   }
 });
