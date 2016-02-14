eafc2d72d76791623ad4885f7f977b7ed9d4e332
jorgegilramos <jorgegilramos@gmail.com>
2016-02-14 23:47:38 +0100

    Fixed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var TestWeb = window.TestWeb || (window.TestWeb = {});
 var $                   = require('jquery');
 global.jQuery           = $;
 window.$                = $;
-//var _                   = require('underscore');
+var _                   = require('underscore');
 var Backbone            = require('backbone');
 Backbone.$ = $;
 //var Platform            = require('platform');
